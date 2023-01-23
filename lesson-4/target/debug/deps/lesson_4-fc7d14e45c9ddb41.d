d7e696e81ffd44982c4962ee9bc40472e9b89022
ShadoWing1 <oyungemer14@gmail.com>
2023-01-23 13:30:23 +0300

    .

---
+++ lesson-4/target/debug/deps/lesson_4-fc7d14e45c9ddb41.d
@@ -0,0 +1,5 @@
+/home/shadowing14/Masaüstü/code/rust/lessons/lesson-4/target/debug/deps/lesson_4-fc7d14e45c9ddb41: src/main.rs
+
+/home/shadowing14/Masaüstü/code/rust/lessons/lesson-4/target/debug/deps/lesson_4-fc7d14e45c9ddb41.d: src/main.rs
+
+src/main.rs:
---
+++ lesson-4/target/debug/lesson-4.d
@@ -0,0 +1,1 @@
+/home/shadowing14/Masaüstü/code/rust/lessons/lesson-4/target/debug/lesson-4: /home/shadowing14/Masaüstü/code/rust/lessons/lesson-4/src/main.rs
